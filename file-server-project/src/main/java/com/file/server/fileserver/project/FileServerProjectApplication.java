d2262849f703a7578203ae88eee4c9a53a00d442
Kelvin Akyea <akyeaniv@gmail.com>
2024-05-14 14:49:23 +0000

    Setup updates

---
+++ file-server-project/src/main/java/com/file/server/fileserver/project/FileServerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.file.server.fileserver.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileServerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileServerProjectApplication.class, args);
+	}
+
+}
---
+++ file-server-project/src/test/java/com/file/server/fileserver/project/FileServerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.file.server.fileserver.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileServerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
